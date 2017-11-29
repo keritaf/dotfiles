d56108517f1d3598c7111eaab2243b6390e38487
Igor Antonov <keritaf@gmail.com>
2017-11-29 12:20:27 +0300

    tmux config updates

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -nfs "`pwd`/.tmux.conf" ~/.tmux.conf
+ln -nfs "`pwd`/tmux.conf" ~/.tmux.conf
 
 mkdir ~/.byobu
-ln -nfs "`pwd`/.tmux.conf" ~/.byobu/.tmux.conf
+ln -nfs "`pwd`/tmux.conf" ~/.byobu/.tmux.conf
 
